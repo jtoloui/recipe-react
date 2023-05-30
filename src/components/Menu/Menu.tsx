558b42a9f7396426c7f1ec1e4ebfca7b0a9c57f3
Jamie Toloui <jamie@toloui.com>
2023-05-30 23:54:43 +0100

    updated menu bar

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,50 +1,75 @@
 import Logo from '@/assets/Logo';
-import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Menu = () => {
 	const location = useLocation();
+	const navigate = useNavigate();
+	const { logout, user } = useAuth0();
 	const [dropdown, setDropdown] = useState(false);
+	const dropdownRef = useRef<null | HTMLButtonElement>(null);
+
 
 	const handleDropdown = () => setDropdown(!dropdown);
 
-	const handleSearch = () => {
+	const handleSearch = (event: MouseEvent<HTMLElement>) => {
+
+		event.preventDefault();
 		console.log('Search');
 	}
 
+
+	const handleClickOutside = (event: WindowEventMap['click']) => {
+		if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+			setDropdown(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('click', handleClickOutside);
+		return () => {
+			document.removeEventListener('click', handleClickOutside);
+		};
+	}, []);
+
 	if (location.pathname === '/login') return null;
 
+
+	const initials = user?.name?.split(' ').map((n: string) => n[0]).join('');
+
+
 	return (
-		<nav className="sticky top-0 z-10 flex items-center justify-between px-20 h-20 bg-white border-b-2 shadow-lg">
-			<div className="flex-1">
+		<nav className="sticky top-0 z-10 h-20 bg-white border-b border-gray-200 flex items-center justify-between px-4 sm:px-6 lg:px-8">
+			{/* logo */}
+			<div className="flex items-center hover:cursor-pointer" onClick={() => navigate("/")}>
 				<Logo />
 			</div>
-			<div className="relative w-full max-w-md mx-auto lg:mx-0">
-				<span className="absolute inset-y-0 left-0 flex items-center pl-3">
-					<svg className="h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-						<path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-					</svg>
-				</span>
-				<input className="block w-full border-b border-gray-300 border-0 py-2 pl-10 pr-4 text-sm placeholder-gray-500 focus:bg-white focus:placeholder-gray-600 focus:text-gray-900 focus:outline-none" placeholder="Search" />
-			</div>
-			<div className="relative flex-1 text-right">
-				<div className="ml-auto mr-5">
-					<button onClick={handleDropdown} className="bg-green rounded-full w-10 h-10 flex items-center justify-center">
-						{/* Profile Circle Icon Here */}
-						{/* Example: */}
-						{/* If user's profile picture exists: */}
-						{/* <img className="h-8 w-8 rounded-full" src="user-profile-picture-url" alt="User's Initials"/> */}
-						{/* Else, display user's initials: */}
-						<span className="text-sm font-medium text-white">JT</span>
-					</button>
-					{dropdown && (
-						<div className="absolute right-0 mt-2 w-48 bg-white rounded-md overflow-hidden shadow-xl z-10">
-							<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Profile</a>
-							<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
-							<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Logout</a>
-						</div>
-					)}
+			<div className="flex items-center justify-center flex-grow">
+				{/* search bar */}
+				<div className="relative flex items-center w-full max-w-md mx-auto border-b-2 border-brownGrey">
+					<span className="absolute inset-y-0 left-0 pl-3 flex items-center hover:cursor-pointer" onClick={handleSearch}>
+						<svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+							<path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+						</svg>
+					</span>
+					<input onClick={handleSearch} className="block w-full border-gray-300 rounded-md leading-5 bg-white text-gray-900 placeholder-gray-500 focus:outline-none   py-2 pl-10 pr-4 text-sm" placeholder="Search Recipe, Profile, or Ingredients" />
 				</div>
+			</div>
+			{/* profile dropdown */}
+			<div className="relative">
+				<button onClick={handleDropdown} className="p-1 rounded-full w-10 h-10 bg-green max-w-xs" ref={dropdownRef}>
+					{/* Profile Circle Icon Here */}
+					<span className="sr-only">Open user menu</span>
+					<span className="text-white font-bold uppercase">{initials}</span>
+				</button>
+				{dropdown && (
+					<div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5">
+						<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Profile</a>
+						<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
+						<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" onClick={() => logout()}>Logout</a>
+					</div>
+				)}
 			</div>
 		</nav>
 	);
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,10 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "../../components/Button";
+
 
 export const Home = () => {
-	const { logout, getAccessTokenSilently } = useAuth0();
+
 	return (
 		<div>
 			Home
-			<Button
-				text="Logout"
-				onClick={() =>
-					logout({
-						logoutParams: {
-							returnTo: `${window.location.origin}`,
-						},
-					})
-				}
-			/>
 		</div>
 	);
 };

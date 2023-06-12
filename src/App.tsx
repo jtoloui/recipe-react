94c801d9d78ac89843914793f7f59813b1562034
Jamie Toloui <jamie@toloui.com>
2023-06-12 20:50:56 +0100

    revert chunk splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import { QueryClient } from 'react-query';
 import {
   Navigate,
@@ -7,27 +7,27 @@ import {
 } from 'react-router-dom';
 
 import { ProtectedRouteWithNav } from '@/components/ProtectedRouteWithNav';
-// import { Home } from '@/pages/Home';
-// import { Login } from '@/pages/Login';
-import { loader as recipeLoader } from '@/pages/RecipeById';
-// import { Profile } from '@/pages/Profile';
+import { Home } from '@/pages/Home';
+import { Login } from '@/pages/Login';
+import { Profile } from '@/pages/Profile';
+import RecipeById, { loader as recipeLoader } from '@/pages/RecipeById';
 import { Welcome } from '@/pages/Welcome';
 
-const Profile = React.lazy(() =>
-  import('@/pages/Profile').then((module) => ({ default: module.Profile }))
-);
-const RecipeById = React.lazy(() => import('@/pages/RecipeById'));
-const Login = React.lazy(() =>
-  import('@/pages/Login').then((module) => ({ default: module.Login }))
-);
-
-// const Welcome = React.lazy(() =>
-//   import('@/pages/Welcome').then((module) => ({ default: module.Welcome }))
+// const Profile = React.lazy(() =>
+//   import('@/pages/Profile').then((module) => ({ default: module.Profile }))
+// );
+// const RecipeById = React.lazy(() => import('@/pages/RecipeById'));
+// const Login = React.lazy(() =>
+//   import('@/pages/Login').then((module) => ({ default: module.Login }))
 // );
 
-const Home = React.lazy(() =>
-  import('@/pages/Home').then((module) => ({ default: module.Home }))
-);
+// // const Welcome = React.lazy(() =>
+// //   import('@/pages/Welcome').then((module) => ({ default: module.Welcome }))
+// // );
+
+// const Home = React.lazy(() =>
+//   import('@/pages/Home').then((module) => ({ default: module.Home }))
+// );
 
 const queryClient = new QueryClient();
 

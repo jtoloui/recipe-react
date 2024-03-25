dd2b12c7c16d744c300c5896aac1fe8e50261f8c
Jamie Toloui <jamie@toloui.com>
2024-03-25 04:40:32 +0000

    adjust chunk splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
-import { defineConfig } from 'vite';
-import preload from 'vite-plugin-preload';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+// import preload from 'vite-plugin-preload';
 import svgr from 'vite-plugin-svgr';
 
 const appEnv = process.env.APP_ENV;
@@ -18,6 +18,32 @@ export default defineConfig({
   },
   build: {
     sourcemap: isDev,
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('@zod') || id.includes('zod')) {
+            return '@zod';
+          }
+
+          if (id.includes('react-hook-form')) {
+            return '@react-hook-form';
+          }
+
+          if (id.includes('fontawesome')) {
+            return '@fontawesome';
+          }
+          // creating a chunk to react routes deps. Reducing the vendor chunk size
+          if (
+            id.includes('react-router-dom') ||
+            id.includes('@remix-run') ||
+            id.includes('react-router') ||
+            id.includes('react-dom')
+          ) {
+            return '@react-router';
+          }
+        },
+      },
+    },
   },
   optimizeDeps: {
     include: ['react-query', 'react-router-dom'],
@@ -27,7 +53,7 @@ export default defineConfig({
     svgr({
       exportAsDefault: false,
     }),
-    preload(),
+    splitVendorChunkPlugin(),
   ],
   server: {
     port: 3000,
@@ -50,6 +76,10 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+    },
+    headers: {
+      'Content-Security-Policy':
+        "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: https://*.unsplash.com https://d35qwubl2aivrw.cloudfront.net; font-src 'self'; connect-src 'self' https://localhost:3001;",
     },
   },
 });
